600a6bbc6bee331917a06f57f7d516566e6b9200
Xargin <cao1988228@163.com>
2019-02-04 17:30:06 +0800

    left association done

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate pest;
 extern crate pest_derive;
 
 extern crate serde_json;
+use pest::prec_climber::{Assoc, Operator, PrecClimber};
 use serde_json::json;
 
 //use pest::error::Error;
@@ -22,45 +23,11 @@ pub struct ParseError {
 /// convert user input to Elasticsearch DSL
 /// example :
 /// ```
-/// convert("a = 1 and (b = 2 and (c = 3)".to_string(), 0, 100, vec![], vec![])
+/// extern crate elastic_query;
+/// use elastic_query::convert;
+/// convert("a = 1 and b = 2 and c = 3".to_string(), 0, 1000, vec![], vec![]);
 /// ```
 /// will generate result :
-/// ```
-/// {
-///	"from": 0,
-///	"query": {
-///		"bool": {
-///			"must": [{
-///				"match": {
-///					"a": {
-///						"query": "1",
-///						"type": "phrase"
-///					}
-///				}
-///			}, {
-///				"bool": {
-///					"must": [{
-///						"match": {
-///							"b": {
-///								"query": "2",
-///								"type": "phrase"
-///							}
-///						}
-///					}, {
-///						"match": {
-///							"c": {
-///								"query": "3",
-///								"type": "phrase"
-///							}
-///						}
-///					}]
-///				}
-///			}]
-///		}
-///	},
-///	"size": 100
-///}
-/// ```
 pub fn convert(
     query: String,
     from: i32,
@@ -71,7 +38,8 @@ pub fn convert(
     let parse_result = ExprParser::parse(Rule::expr, query.as_str());
     match parse_result {
         Ok(mut expr_ast) => {
-            let dsl = walk_tree(expr_ast.next().unwrap(), true);
+            let ast = generate_ast(expr_ast.next().unwrap());
+            let dsl = walk_tree(ast, true);
 
             let mut result = json!({
                "query": dsl,
@@ -137,47 +105,69 @@ fn build_sort(sort: Vec<&str>) -> serde_json::Value {
 
 use pest::iterators::Pair;
 
-fn walk_tree(record: Pair<Rule>, is_root: bool) -> serde_json::Value {
-    match record.clone().as_rule() {
-        Rule::bool_expr | Rule::expr | Rule::paren_bool => {
-            return walk_tree(record.into_inner().next().unwrap(), is_root);
-        }
-        Rule::or_expr => {
-            let mut iter = record.into_inner();
-            let (left_val, right_val) = (
-                walk_tree(iter.next().unwrap(), false),
-                walk_tree(iter.next().unwrap(), false),
-            );
+#[derive(Debug)]
+enum Expression {
+    CompExpr(String, Rule, String),
+    AndExpr(Box<Expression>, Box<Expression>),
+    OrExpr(Box<Expression>, Box<Expression>),
+}
 
-            return serde_json::json!({
-                "bool" : {
-                    "should" : [left_val, right_val]
-                }
-            });
-        }
-        Rule::and_expr => {
-            let mut iter = record.into_inner();
-            let (left_val, right_val) = (
-                walk_tree(iter.next().unwrap(), false),
-                walk_tree(iter.next().unwrap(), false),
-            );
+fn generate_ast(pair: Pair<Rule>) -> Expression {
+    let climber = PrecClimber::new(vec![
+        Operator::new(Rule::and_op, Assoc::Left) | Operator::new(Rule::or_op, Assoc::Left),
+    ]);
 
+    consume(pair, &climber)
+}
+
+fn consume(pair: Pair<Rule>, climber: &PrecClimber<Rule>) -> Expression {
+    let atom = |pair| consume(pair, climber);
+    let infix = |lhs, op: Pair<Rule>, rhs| match op.as_rule() {
+        Rule::and_op => Expression::AndExpr(Box::new(lhs), Box::new(rhs)),
+        Rule::or_op => Expression::OrExpr(Box::new(lhs), Box::new(rhs)),
+        _ => unreachable!(),
+    };
+
+    match pair.as_rule() {
+        Rule::expr => {
+            let pairs = pair.into_inner();
+            climber.climb(pairs, atom, infix)
+        }
+        Rule::paren_bool => pair.into_inner().next().map(atom).unwrap(),
+        Rule::comp_expr => {
+            let mut iter = pair.into_inner();
+            let (lhs, op, rhs) = (
+                iter.next().unwrap().as_str().to_string(),
+                iter.next().unwrap().into_inner().next().unwrap().as_rule(),
+                iter.next().unwrap().as_str().to_string(),
+            );
+            return Expression::CompExpr(lhs, op, rhs);
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn walk_tree(expr: Expression, is_root: bool) -> serde_json::Value {
+    match expr {
+        Expression::AndExpr(lexpr, rexpr) => {
+            let (left_val, right_val) = (walk_tree(*lexpr, false), walk_tree(*rexpr, false));
             return serde_json::json!({
                 "bool" : {
                     "must" : [left_val, right_val]
                 }
             });
         }
-        Rule::comp_expr => {
-            let mut iter = record.into_inner();
-            let (lhs, op, rhs) = (
-                iter.next().unwrap().as_str().to_string(),
-                iter.next().unwrap().into_inner().next().unwrap().as_rule(),
-                iter.next().unwrap().as_str().to_string(),
-            );
-
+        Expression::OrExpr(lexpr, rexpr) => {
+            let (left_val, right_val) = (walk_tree(*lexpr, false), walk_tree(*rexpr, false));
+            return serde_json::json!({
+                "bool" : {
+                    "should" : [left_val, right_val]
+                }
+            });
+        }
+        Expression::CompExpr(lhs, operator, rhs) => {
             #[rustfmt::skip]
-            let result = match op {
+            let result = match operator {
                 Rule::eq | Rule::like => json!({"match" : {lhs : {"query" : rhs, "type" : "phrase"}}}),
                 Rule::gte => json!({"range" : {lhs : {"from" : rhs}}}),
                 Rule::lte => json!({"range" : {lhs : {"to" : rhs}}}),
@@ -213,18 +203,17 @@ fn walk_tree(record: Pair<Rule>, is_root: bool) -> serde_json::Value {
             }
             return result;
         }
-        _ => unreachable!(),
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::convert;
     use serde_json::json;
 
     struct TestCase<'a> {
         input: (&'a str, i32, i32, Vec<&'a str>, Vec<&'a str>), // query, from, size, sort, agg
         output: serde_json::Value,
+        comment: &'a str,
     }
 
     #[test]
@@ -233,34 +222,46 @@ mod tests {
             TestCase {
                 input: ("a=1", 1000, 1000, vec![], vec![]),
                 output: json!({"query" : {"bool" : {"must" : [{"match" :{"a" : {"query" : "1", "type" : "phrase"}}}]}}, "from" : 1000, "size" : 1000}),
+                comment: "equal expression test",
             },
             TestCase {
                 input: ("a=1", 1000, 1000, vec!["a asc", "b desc"], vec![]),
                 output: json!({"from":1000,"query":{"bool":{"must":[{"match":{"a":{"query":"1","type":"phrase"}}}]}},"size":1000,"sort":[{"a":"asc"},{"b":"desc"}]}),
+                comment: "sort test",
             },
             TestCase {
                 input: ("a=1", 0, 1000, vec!["a asc", "b"], vec![]),
                 output: json!({"from":0,"query":{"bool":{"must":[{"match":{"a":{"query":"1","type":"phrase"}}}]}},"size":1000,"sort":[{"a":"asc"},{"b":"asc"}]}),
+                comment: "sort test",
             },
             TestCase {
                 input: ("a in (1,2,3)", 1000, 1000, vec![], vec![]),
                 output: json!({"from":1000,"query":{"bool":{"must":[{"terms":{"a":["1","2","3"]}}]}},"size":1000}),
+                comment: "in expression test",
             },
             TestCase {
                 input: ("a in (   1, 2,  3)", 1000, 1000, vec![], vec![]),
                 output: json!({"from":1000,"query":{"bool":{"must":[{"terms":{"a":["1","2","3"]}}]}},"size":1000}),
+                comment: "auto trim space test",
             },
             TestCase {
                 input: ("a in (   1, 2,  3)", 1000, 1000, vec![], vec!["a", "b"]),
                 output: json!({"aggregations":{"a":{"aggregations":{"b":{"terms":{"field":"b","size":0}}},"terms":{"field":"a","size":200}}},"from":1000,"query":{"bool":{"must":[{"terms":{"a":["1","2","3"]}}]}},"size":1000}),
+                comment: "aggregation test",
             },
             TestCase {
                 input: ("a = 1 and (b = 2 and (c = 3))", 0, 1000, vec![], vec![]),
                 output: json!({"from":0,"query":{"bool":{"must":[{"match":{"a":{"query":"1","type":"phrase"}}},{"bool":{"must":[{"match":{"b":{"query":"2","type":"phrase"}}},{"match":{"c":{"query":"3","type":"phrase"}}}]}}]}},"size":1000}),
+                comment: "paren expr test",
+            },
+            TestCase {
+                input: ("a = 1 and b = 2 and c = 3", 0, 1000, vec![], vec![]),
+                output: json!({"from":0,"query":{"bool":{"must":[{"bool":{"must":[{"match":{"a":{"query":"1","type":"phrase"}}},{"match":{"b":{"query":"2","type":"phrase"}}}]}},{"match":{"c":{"query":"3","type":"phrase"}}}]}},"size":1000}),
+                comment: "left association test",
             },
         ];
         test_cases.iter().for_each(|case| {
-            let output = convert(
+            let output = super::convert(
                 case.input.0.to_string(),
                 case.input.1,
                 case.input.2,
